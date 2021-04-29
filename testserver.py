da9a13ac99cbe3c759bf1e64e3c80552c7393d60
Hamed Babaei <hamedbabaeigiglou@gmail.com>
2021-04-29 18:36:21 +0430

    update

--- testserver.py
+++ testserver.py
@@ -1,8 +1,8 @@
 from roberta import Roberta
 
-en = "RT #USER#: Funny how “15 days to slow the spread” turned into “maybe you can have a barbecue in July"
+en = "RT #USER#: Funny how “15 days to slow the spread” turned into “maybe you can have a barbecue in July. RT #USER#: Funny how “15 days to slow the spread” turned into “maybe you can have a barbecue in July. RT #USER#: Funny how “15 days to slow the spread” turned into “maybe you can have a barbecue in July."
 print("Result for English:\n", Roberta(lang='en').transform([en]))
 
-es = "RT #USER#: Esperando para ver la captura de Jaime Perla. \n #USER# #USER# Que bueno Romeo con la ayud"
+es = "RT #USER#: Esperando para ver la captura de Jaime Perla. \n #USER# #USER# Que bueno Romeo con la ayud. RT #USER#: Esperando para ver la captura de Jaime Perla. \n #USER# #USER# Que bueno Romeo con la ayud . RT #USER#: Esperando para ver la captura de Jaime Perla. \n #USER# #USER# Que bueno Romeo con la ayud"
 print("Result for Spanish:\n", Roberta(lang='es').transform([es]))
 
